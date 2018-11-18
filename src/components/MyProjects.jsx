9660cbe21ada561312eb09a76883da7c905a4558
Brytner Evangelista <brytnerevangelista@gmail.com>
2018-11-17 22:22:43 -0500

    My Project Component added

---
+++ src/components/MyProjects.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class MyProjects extends Component {
+  state = {};
+  render() {
+    return (
+      <h1>This is the beggining of a list of projects using JavaScript</h1>
+    );
+  }
+}
+
+export default MyProjects;
